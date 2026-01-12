6ecd4c4500b543afd70949e930789a565f342b2b
Srishti Lamba <72405276+srishti-lamba@users.noreply.github.com>
2026-01-11 20:39:50 -0500

    Buttons

--- src/components/settings.tsx
+++ src/components/settings.tsx
@@ -42,20 +42,16 @@ export default function Settings() {
               className="popup"
             >
 
-              <svg>
-                <defs>
-                  <clipPath id="two-diamonds"  clipPathUnits="objectBoundingBox">
-                    <path d="m 0 30 L 30 0 L 49 19 L 68 0 L 98 30 L 68 60 L 49 41 L 30 60 Z 
-                      m 36 20 L 47 39 L 38 30 L 47 21 L 36 10 L 16 30 Z 
-                      M 51 21 L 60 30 L 51 39 L 62 50 L 82 30 L 62 10 L 51 21 Z" />
-                  </clipPath>
-                </defs>
-              </svg>
-
               <h1>Settings</h1>
-              <span className="section-wrapper">
-                <span className="section">Difficulty</span>
-              </span>
+                <span className="section">
+                  <span className="prompt">Difficulty</span>
+                  <span className="buttons">
+                  <button>Easy</button>
+                  <button>Normal</button>
+                  <button>Hard</button>
+                  </span>
+
+                </span>
             </div>
 
           </>
